2b726c219987328684c509642640e6993c873f43
CRahne <coleprahne@gmail.com>
2019-12-18 20:39:46 -0600

    Created Mark1, and also made some code for it

--- AutonomousAlgorithmMark1/src/main/java/frc/robot/Robot.java
+++ AutonomousAlgorithmMark1/src/main/java/frc/robot/Robot.java
@@ -1,131 +1,105 @@
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
-
 package frc.robot;
 
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
+
+import edu.wpi.first.wpilibj.Compressor;
+import edu.wpi.first.wpilibj.SpeedControllerGroup;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Scheduler;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.commands.ExampleCommand;
 import frc.robot.subsystems.ExampleSubsystem;
 
-/**
- * The VM is configured to automatically run this class, and to call the
- * functions corresponding to each mode, as described in the TimedRobot
- * documentation. If you change the name of this class or the package after
- * creating this project, you must also update the build.gradle file in the
- * project.
- */
 public class Robot extends TimedRobot {
   public static ExampleSubsystem m_subsystem = new ExampleSubsystem();
   public static OI m_oi;
+  
+  public static Compressor comp = new Compressor();
 
-  Command m_autonomousCommand;
-  SendableChooser<Command> m_chooser = new SendableChooser<>();
+  public static WPI_TalonSRX DrivetrainLeftMaster = new WPI_TalonSRX(0);
+  public static WPI_VictorSPX DrivetrainLeftSlave = new WPI_VictorSPX(1);
+  public static WPI_TalonSRX DrivetrainRightMaster = new WPI_TalonSRX(2);
+  public static WPI_VictorSPX DrivetrainRightSlave = new WPI_VictorSPX(3);
 
-  /**
-   * This function is run when the robot is first started up and should be
-   * used for any initialization code.
-   */
-  @Override
+  public static SpeedControllerGroup Left = new SpeedControllerGroup(DrivetrainLeftMaster, DrivetrainLeftSlave);
+  public static SpeedControllerGroup Right = new SpeedControllerGroup(DrivetrainRightMaster, DrivetrainRightSlave);
+
+  public static DifferentialDrive drive = new DifferentialDrive(Left, Right);
+
+  // https://first.wpi.edu/FRC/roborio/beta/docs/java/edu/wpi/first/wpilibj/Timer.html - Timer Docs
+
+  
+
+
   public void robotInit() {
     m_oi = new OI();
-    m_chooser.setDefaultOption("Default Auto", new ExampleCommand());
-    // chooser.addOption("My Auto", new MyAutoCommand());
-    SmartDashboard.putData("Auto mode", m_chooser);
+
   }
 
-  /**
-   * This function is called every robot packet, no matter the mode. Use
-   * this for items like diagnostics that you want ran during disabled,
-   * autonomous, teleoperated and test.
-   *
-   * <p>This runs after the mode specific periodic functions, but before
-   * LiveWindow and SmartDashboard integrated updating.
-   */
-  @Override
+
   public void robotPeriodic() {
   }
 
-  /**
-   * This function is called once each time the robot enters Disabled mode.
-   * You can use it to reset any subsystem information you want to clear when
-   * the robot is disabled.
-   */
-  @Override
+
   public void disabledInit() {
   }
 
-  @Override
+
   public void disabledPeriodic() {
     Scheduler.getInstance().run();
   }
 
-  /**
-   * This autonomous (along with the chooser code above) shows how to select
-   * between different autonomous modes using the dashboard. The sendable
-   * chooser code works with the Java SmartDashboard. If you prefer the
-   * LabVIEW Dashboard, remove all of the chooser code and uncomment the
-   * getString code to get the auto name from the text box below the Gyro
-   *
-   * <p>You can add additional auto modes by adding additional commands to the
-   * chooser code above (like the commented example) or additional comparisons
-   * to the switch structure below with additional strings & commands.
-   */
-  @Override
+
+  // Time Constants
+  public static double TIME_init = Timer.getFPGATimestamp();
+  public static double TIME_total = 10;
+
+  // Voltage Constants
+  public static double VOLTS_max = 0.8;
+  public static double VOLTS_min = 0.3;
+
+  // Created Constants
+  public static double TIME_half = TIME_total / 2;
+  public static double gain = (VOLTS_min - VOLTS_max) / Math.pow((TIME_init - TIME_half), 2);
+  public double TIME_curr;
+  public double VOLTS_curr;
+  
+  public double getNewVoltCurr(double TIME_curr) {
+    return (gain * (Math.pow(TIME_curr - TIME_half, 2))) + VOLTS_max;
+  }
   public void autonomousInit() {
-    m_autonomousCommand = m_chooser.getSelected();
-
-    /*
-     * String autoSelected = SmartDashboard.getString("Auto Selector",
-     * "Default"); switch(autoSelected) { case "My Auto": autonomousCommand
-     * = new MyAutoCommand(); break; case "Default Auto": default:
-     * autonomousCommand = new ExampleCommand(); break; }
-     */
-
-    // schedule the autonomous command (example)
-    if (m_autonomousCommand != null) {
-      m_autonomousCommand.start();
-    }
+    TIME_curr = Timer.getFPGATimestamp();
+    VOLTS_curr = getNewVoltCurr(TIME_curr);
+    drive.arcadeDrive(VOLTS_curr, 0);
+    SmartDashboard.putNumber("Current Volts", VOLTS_curr);
+    SmartDashboard.putNumber("Time_curr", TIME_curr);
+    SmartDashboard.putNumber("Gain", gain);
   }
 
-  /**
-   * This function is called periodically during autonomous.
-   */
-  @Override
+
   public void autonomousPeriodic() {
-    Scheduler.getInstance().run();
+    TIME_curr = Timer.getFPGATimestamp();
+    VOLTS_curr = getNewVoltCurr(TIME_curr);
+    drive.arcadeDrive(VOLTS_curr, 0);
+    SmartDashboard.putNumber("Current Volts", VOLTS_curr);
+    SmartDashboard.putNumber("Time_curr", TIME_curr);
   }
 
-  @Override
+
   public void teleopInit() {
-    // This makes sure that the autonomous stops running when
-    // teleop starts running. If you want the autonomous to
-    // continue until interrupted by another command, remove
-    // this line or comment it out.
-    if (m_autonomousCommand != null) {
-      m_autonomousCommand.cancel();
-    }
+    comp.start();
   }
 
-  /**
-   * This function is called periodically during operator control.
-   */
-  @Override
+
   public void teleopPeriodic() {
     Scheduler.getInstance().run();
   }
 
-  /**
-   * This function is called periodically during test mode.
-   */
-  @Override
   public void testPeriodic() {
   }
 }
